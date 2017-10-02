7644c1149d2775dd22a4fdec6c3ee62cb33df27c
rb <rbtheroot@gmail.com>
2017-10-02 09:52:15 +0530

    Added functionality to fetch two api data

--- src/components/menuItems.js
+++ src/components/menuItems.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
 import 'aframe';
 import 'aframe-layout-component';
 import 'aframe-event-set-component';
 import {Entity} from 'aframe-react';
 import 'aframe-look-at-component';
+import React, { Component } from 'react';
+
 
 
 const url = 'https://randomuser.me/api/?results=5';
@@ -12,8 +13,8 @@ const url1 = 'https://randomuser.me/api/?results=9';
 
 export default class UserList extends Component {
     constructor(props) {
-      super(props);
-        this.handleClick = this.handleClick.bind(this);
+      super(props);      
+      this.handleClick = this.handleClick.bind(this);
       this.state = {
         person: [],
         menuCat1: []};
@@ -37,25 +38,28 @@ export default class UserList extends Component {
         .then((data) => {
              this.setState({ menuCat1: data.results });
         });
+    }
+    
+    handleClick(cubeId){
+      console.log(cubeId);
 
     }
-
-    handleClick() {
-    }
-  
     render() {
       const persons = this.state.person.map((item, i) => {
         return <Entity 
                     key={item.email} 
-                    mixin="cube"
+                    id={`cube${i}`}
                     event-set__1="_event: click; _target: #cubes1; visible: true"
-                    event-set__3="_event: mouseenter; material.color: #5A67A6"
-                    event-set__4="_event: mouseleave; material.color: #FCB241">
+                    mixin="cube"
+                    >
                 </Entity>
       });
 
       const mCat1 = this.state.menuCat1.map((item, i) => {
-        return <Entity key={item.email}  mixin="cube"></Entity>
+        return <Entity 
+                  key={item.email}  
+                  mixin="cube">
+                </Entity>
       });
 
       return (
@@ -67,8 +71,13 @@ export default class UserList extends Component {
               event-set__4="_event: mouseleave; material.color: #FCB241"
               position="0 1 -2"
               scale={{x: 0.1, y: 0.1, z: 0.1}}>
-        <Entity id="cubes" visible="false" layout="type: circle; radius: 3;" position="0 1 -9" look-at="#look-cam" >
-          {persons}
+        <Entity 
+            id="cubes" 
+            visible="false" 
+            layout="type: circle; radius: 3;" 
+            position="0 1 -9" 
+            look-at="#look-cam" >
+                      {persons}
         </Entity>
 
         <Entity id="cubes1" visible="false" layout="type: circle; radius: 3;" position="1 3 -1" look-at="#look-cam" >
